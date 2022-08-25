06d82e5729622977a571f58d202cde13338a5346
Runal Banarse <65166862+runalb@users.noreply.github.com>
2022-08-25 22:07:18 +0530

    YT Video Downloader

---
+++ YT_Video_Downloader.py
@@ -0,0 +1,19 @@
+from pytube import YouTube
+
+url = input("Enter YT Video URL: ")
+ytv = YouTube(url)
+print(f'\n Video Title: {ytv.title}')
+
+ytv_streams = ytv.streams.all()
+ytv_streams_li = list((enumerate(ytv_streams)))
+
+print("\n Stream lists:")
+for stream in ytv_streams_li:
+    print(stream)
+print()
+
+strm_no = int(input("Enter stream no: "))
+
+print(f' Downloading...:\n {ytv_streams[strm_no]} ')
+ytv_streams[strm_no].download()
+print(f'\n Download Completed...:\n {ytv.title} \n {ytv_streams[0]}')
